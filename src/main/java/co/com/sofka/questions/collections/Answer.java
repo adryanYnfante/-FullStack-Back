e531bfb723ad0182002de09e39435c6c0ff31ad3
Nirsch95 <castromanzano95@gmail.com>
2023-01-31 11:26:47 -0500

    Conexión a MongoDB

--- src/main/java/co/com/sofka/questions/collections/Answer.java
+++ src/main/java/co/com/sofka/questions/collections/Answer.java
@@ -3,7 +3,7 @@ package co.com.sofka.questions.collections;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document
+@Document(value = "answers")
 public class Answer {
     @Id
     private String id;
