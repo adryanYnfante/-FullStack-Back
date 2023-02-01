388b79e23530fe0fd347390d993866e49ab73198
Oscardev <121882462+oscardeveloper2000@users.noreply.github.com>
2023-02-01 12:35:14 -0500

    Punto de contro, Paginator OK

--- src/main/java/co/com/sofka/questions/usecases/ListUseCase.java
+++ src/main/java/co/com/sofka/questions/usecases/ListUseCase.java
@@ -2,9 +2,13 @@ package co.com.sofka.questions.usecases;
 
 import co.com.sofka.questions.model.QuestionDTO;
 import co.com.sofka.questions.reposioties.QuestionRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.function.Supplier;
 
@@ -23,6 +27,20 @@ public class ListUseCase implements Supplier<Flux<QuestionDTO>> {
     public Flux<QuestionDTO> get() {
         return questionRepository.findAll()
                 .map(mapperUtils.mapEntityToQuestion());
+    }
+    //
+    public Flux<QuestionDTO> getPage(int page) {
+        Pageable pageable = PageRequest.of(page, 10);
+        return questionRepository.findAllByIdNotNullOrderByIdAsc(pageable)
+                .map(mapperUtils.mapEntityToQuestion());
+    }
+    //
+    public Mono<Integer> getTotalPages() {
+        return questionRepository.count().map(count ->(int) Math.ceil(count/10D));
+    }
+
+    public Mono<Long> getTotalQuestions() {
+        return questionRepository.findAll().count();
     }
 
 }
