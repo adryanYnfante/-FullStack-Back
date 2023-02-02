f2b28d70e94fdf2c8c59bf46dd31aec48d2b23f5
dapevi1997 <dapevi97@gmail.com>
2023-02-02 18:31:54 -0500

    Tests Ok, falta AddAnswer

--- src/test/java/co/com/sofka/questions/usecases/GetUseCaseTest.java
+++ src/test/java/co/com/sofka/questions/usecases/GetUseCaseTest.java
@@ -1,7 +1,62 @@
 package co.com.sofka.questions.usecases;
 
-import static org.junit.jupiter.api.Assertions.*;
+import co.com.sofka.questions.collections.Question;
+import co.com.sofka.questions.model.AnswerDTO;
+import co.com.sofka.questions.model.QuestionDTO;
+import co.com.sofka.questions.reposioties.AnswerRepository;
+import co.com.sofka.questions.reposioties.QuestionRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class GetUseCaseTest {
+
+    @SpyBean
+    GetUseCase getUseCase;
+    @Autowired
+    MapperUtils mapperUtils;
+    @Mock
+    private QuestionRepository questionRepository;
+
+
+
+    @Test
+    public void getUseCaseTest(){
+        QuestionDTO questionDto = new QuestionDTO("idQuestion","300","daniel","acción","acción");
+
+
+        AnswerDTO answerDTO = new AnswerDTO("idAnswer","300","idUser","answer",5);
+
+
+        List<AnswerDTO> answerDTOs = new ArrayList<>();
+        answerDTOs.add(answerDTO);
+
+        questionDto.setAnswers(answerDTOs);
+
+        Question question = mapperUtils.mapperToQuestion("300").apply(questionDto);
+
+
+        Mono<Question> questionDTOMono = Mono.just(question);
+
+        Mockito.when(questionRepository.findById(anyString())).thenReturn(questionDTOMono);
+
+        StepVerifier.create(getUseCase.apply("idQuestion"))
+                .expectNextMatches(questDTO -> {
+                   return questDTO.getUserId().equals("300") ;
+                })
+                .verifyComplete();
+    }
 
 }
--- src/test/java/co/com/sofka/questions/usecases/UpdateUseCaseTest.java
+++ src/test/java/co/com/sofka/questions/usecases/UpdateUseCaseTest.java
@@ -1,7 +1,55 @@
 package co.com.sofka.questions.usecases;
 
+import co.com.sofka.questions.collections.Question;
+import co.com.sofka.questions.model.AnswerDTO;
+import co.com.sofka.questions.model.QuestionDTO;
+import co.com.sofka.questions.reposioties.QuestionRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class UpdateUseCaseTest {
+
+    @SpyBean
+    private UpdateUseCase updateUseCase;
+    @Autowired
+    private MapperUtils mapperUtils;
+    @Mock
+    QuestionRepository questionRepository;
+
+    @Test
+    public void updateUseCaseTest(){
+        QuestionDTO questionDto = new QuestionDTO("idQuestion","300","daniel","acción","acción");
+
+        Question question = mapperUtils.mapperToQuestion("idQuestion").apply(questionDto);
+
+        Mono<Question> questionMono = Mono.just(question);
+
+
+
+        Question question1 = Mockito.mock(Question.class);
+
+        Mockito.when(questionRepository.save(question1)).thenReturn(questionMono);
+
+        StepVerifier.create(updateUseCase.apply(questionDto))
+                .expectNextMatches(q->{
+
+                    return q.equals("idQuestion");
+                })
+                .verifyComplete();
+
+    }
 
 }
