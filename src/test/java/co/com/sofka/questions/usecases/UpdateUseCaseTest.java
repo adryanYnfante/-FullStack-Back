b1a0dfb1df1f272537d0e4e4adc64d8422b58ee0
dsbpajedrez <96492716+dsbpajedrez@users.noreply.github.com>
2023-02-02 18:12:40 -0500

    New: UpdateCaseTest working

--- src/test/java/co/com/sofka/questions/usecases/UpdateUseCaseTest.java
+++ src/test/java/co/com/sofka/questions/usecases/UpdateUseCaseTest.java
@@ -9,14 +9,62 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class UpdateUseCaseTest {
+    @Mock
+    QuestionRepository repository;
 
+    @Mock
+    UpdateUseCase updateUseCase;
+
+    @Autowired
+    private MapperUtils mapperUtils;
+
+    @Mock
+    QuestionRepository questionRepository;
+
+    @BeforeEach
+    public void setup(){
+        MapperUtils mapperUtils = new MapperUtils();
+        repository = mock(QuestionRepository.class);
+
+    }
+
+    @Test
+    void getValidationTest(){
+        Question question = new Question();
+        QuestionDTO questionDTO =new QuestionDTO("300","132838","Es buena","abierta","peliculas");
+        question.setId(questionDTO.getId());
+        question.setUserId(questionDTO.getUserId());
+        question.setQuestion(questionDTO.getQuestion());
+        question.setType(questionDTO.getType());
+        question.setCategory(questionDTO.getCategory());
+
+
+        Mockito.when(questionRepository.save(question))
+                .thenReturn(
+                        Mono.just(
+                                mapperUtils.mapperToQuestion(
+                                        questionDTO.getId()
+                                        )
+                                        .apply(questionDTO)
+                        )
+                );
+
+        StepVerifier.create(updateUseCase.apply(questionDTO))
+                .expectNextMatches(MonoQ -> {
+                    equals("300");
+                    return true;
+                });
+               // .verifyComplete();
+    }
 
 }
