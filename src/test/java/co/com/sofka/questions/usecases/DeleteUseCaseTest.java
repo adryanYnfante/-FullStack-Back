3bbdc0e6d4fe84c27b19ef1bb329a3dbef6150be
dsbpajedrez <96492716+dsbpajedrez@users.noreply.github.com>
2023-02-02 17:56:46 -0500

    Errase update case test, not working yet

--- src/test/java/co/com/sofka/questions/usecases/DeleteUseCaseTest.java
+++ src/test/java/co/com/sofka/questions/usecases/DeleteUseCaseTest.java
@@ -1,7 +1,58 @@
 package co.com.sofka.questions.usecases;
 
-import static org.junit.jupiter.api.Assertions.*;
+import co.com.sofka.questions.collections.Answer;
+import co.com.sofka.questions.collections.Question;
+import co.com.sofka.questions.model.AnswerDTO;
+import co.com.sofka.questions.model.QuestionDTO;
+import co.com.sofka.questions.reposioties.AnswerRepository;
+import co.com.sofka.questions.reposioties.QuestionRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DeleteUseCaseTest {
+    @SpyBean
+    private DeleteUseCase deleteUseCase;
+    @Mock
+    QuestionRepository questionRepository;
 
+    @Mock
+    AnswerRepository answerRepository;
+    @Test
+    void setDeleteHappyPass(){
+        Question question = new Question();
+        QuestionDTO questionDTO =new QuestionDTO("300","julian2345","accion","accion","accion");
+        question.setId(questionDTO.getId());
+        question.setUserId(questionDTO.getUserId());
+        question.setQuestion(questionDTO.getQuestion());
+        question.setType(questionDTO.getType());
+        question.setCategory(questionDTO.getCategory());
+
+        Mockito.when(questionRepository.deleteById(question.getId()))
+                                .thenReturn(
+                                   Mono.empty());
+        Answer answer = new Answer();
+        AnswerDTO answerDTO= new AnswerDTO("300","132838","ok",5);
+
+        answer.setQuestionId(answerDTO.getQuestionId());
+        answer.setUserId(answerDTO.getUserId());
+        answer.setAnswer(answerDTO.getAnswer());
+        answer.setPosition(answerDTO.getPosition());
+        List<AnswerDTO> answersDTO = new ArrayList<>();
+        answersDTO.add(answerDTO);
+
+        questionDTO.setAnswers(answersDTO);
+        Mockito.when(answerRepository.deleteByQuestionId(question.getId())).thenReturn(Mono.empty());
+            StepVerifier.create(deleteUseCase.apply(questionDTO.getId()))
+                    .expectNext()
+                    .verifyComplete();
+    }
 }
--- src/test/java/co/com/sofka/questions/usecases/UpdateUseCaseTest.java
+++ src/test/java/co/com/sofka/questions/usecases/UpdateUseCaseTest.java
@@ -1,7 +1,22 @@
 package co.com.sofka.questions.usecases;
 
-import static org.junit.jupiter.api.Assertions.*;
+import co.com.sofka.questions.collections.Question;
+import co.com.sofka.questions.model.QuestionDTO;
+import co.com.sofka.questions.reposioties.QuestionRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
+import static org.mockito.Mockito.mock;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class UpdateUseCaseTest {
+
 
 }
