998090cbe51d3788707cad936d3151a18ecf8512
LilianaGallego <lilianagallegom@gmail.com>
2023-02-02 23:10:37 -0500

    new: Se agrega actualizar respuesta

--- src/main/java/co/com/sofka/questions/model/AnswerDTO.java
+++ src/main/java/co/com/sofka/questions/model/AnswerDTO.java
@@ -6,6 +6,7 @@ import java.util.Objects;
 import java.util.Optional;
 
 public class AnswerDTO {
+    private String id;
     @NotBlank(message = "Debe existir el userId para este objeto")
     private String userId;
     @NotBlank
@@ -18,6 +19,22 @@ public class AnswerDTO {
 
     public AnswerDTO() {
 
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public AnswerDTO(String id, String userId, String questionId, String answer, Integer position) {
+        this.id = id;
+        this.userId = userId;
+        this.questionId = questionId;
+        this.answer = answer;
+        this.position = position;
     }
 
     public AnswerDTO(@NotBlank String questionId, @NotBlank String userId, @NotBlank String answer, Integer position) {
