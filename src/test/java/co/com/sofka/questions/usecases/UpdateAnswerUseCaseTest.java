f5bdf1f08fee384752b724327d0a3c158d9a0995
JULIAN MAZO <82735837+BERTOTEM@users.noreply.github.com>
2023-02-03 11:19:13 -0500

    Test de EditarAnswer

---
+++ src/test/java/co/com/sofka/questions/usecases/UpdateAnswerUseCaseTest.java
@@ -0,0 +1,11 @@
+package co.com.sofka.questions.usecases;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class UpdateAnswerUseCaseTest {
+
+    @Test
+    void editAnswer() {
+    }
+}
